c4caa854ec398dfe196eef48273c60419440be05
Edufemartins <edufemartins@yahoo.com.br>
2024-08-28 15:45:41 -0300

    third commit

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 class heroi{
-    constructor(nomeHeroi, tipoHeroi){
-        this.nomeHeroi = nomeHeroi
-        this.tipoHeroi = tipoHeroi
+    constructor(nomeTipo, nomeAtaque){
+        this.nomeTipo = nomeTipo
+        this.nomeAtaque = nomeAtaque
     }
 }
 
-let guerreiro = new heroi("mago", "espada")
+let guerreiro = new heroi(["Mago", "Guerreiro", "Monge", "Ninja"], ["Magia", "Espada", "Artes Marciais", "Shuriken"])
 
-console.log(guerreiro)
+for (let i = 0; i <= guerreiro.length; i++){
+
+    if(i === 0){
+        console.log(guerreiro.nomeTipo[0] + " " + guerreiro.nomeAtaque[0]);
+    }else if(i === 1){
+        console.log(guerreiro.nomeTipo[1] + " " + guerreiro.nomeAtaque[1]);
+    }else if(i === 2){
+        console.log(guerreiro.nomeTipo[2] + " " + guerreiro.nomeAtaque[2]);
+    }else(i === 3){
+        console.log(guerreiro.nomeTipo[3] + " " + guerreiro.nomeAtaque[3]);
+    }
+}
+
+
+
+
+
+
+// console.log(`O "this.nomeHeroi" ) atacou usando "thisAtaque"`)
