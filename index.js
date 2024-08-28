fb602f771fb39eb3c1942f75666d0d47cd98b707
Edufemartins <edufemartins@yahoo.com.br>
2024-08-28 14:47:37 -0300

    second commit

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello Word")
+class heroi{
+    constructor(nomeHeroi, tipoHeroi){
+        this.nomeHeroi = nomeHeroi
+        this.tipoHeroi = tipoHeroi
+    }
+}
+
+let guerreiro = new heroi("mago", "espada")
+
+console.log(guerreiro)
